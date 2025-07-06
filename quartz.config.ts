81f442866fb605f5107e1ad3951ec85f410606ba
RDELX <51673029+RDELX@users.noreply.github.com>
2025-07-06 20:40:26 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "rdelx.github.io/Notes",
+    baseUrl: "rdelx.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
